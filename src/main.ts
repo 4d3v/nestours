be053c2e408cac14b2e77da19a2d745c08d8f968
Jean Marques <4.dev.001@gmail.com>
2020-09-20 23:05:12 -0300

    Added global config for env vars

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TourDifficulty } from './tours/tour-difficulty.enum';
+import * as config from 'config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
